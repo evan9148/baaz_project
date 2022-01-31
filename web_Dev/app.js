c8fb2ca821dfa310f7829272839e8c9f5cc4f205
evan9148 <evannjaline19@navgurukul.org>
2022-01-31 11:55:35 +0530

    pagination api changes

--- web_Dev/app.js
+++ web_Dev/app.js
@@ -1,7 +1,8 @@
 var express = require('express'); 
 var app = express();
 const mongoose = require("mongoose");
-const Product = require("../baazWebDev/product")
+const Product = require("../Web_Dev/product")
+const Str = require('@supercharge/strings')
 const port = 5000;
 
 
@@ -23,19 +24,26 @@ mongoose
 app.use(express.json());
 
 app.get("/search/:title" , (req,res) => {
-    Product.findOne({title : req.params.title}).exec((err,data) => {
-        if (err){
-            console.log(err)
-            res.status(400).json({
-                error : err
-            })
-        }else if (data) {
-            res.status(200).json({
-                My_data : data
-            })
-
+    const {page = 1,limit = 8} = req.query
+    Product.find({
+        title: { $regex: ".*" + req.params.title + ".*" , $options: "i" },
+      }).limit(limit*1)
+        .skip((page - 1) * limit)
+        .exec((err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(400).json({
+            error: err,
+          });
+        } else if (data) {
+          res.status(200).json({
+            message: data.length
+              ? `Found ${data.length} results for the searched term`
+              : `Found nothing`,
+            My_data: data,
+          });
         }
-    })
+      });
 })
 
 
